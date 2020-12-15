3abd364396815406d7a968d89b91338cfee698b9
Aryan Sinha <63485082+techyminati@users.noreply.github.com>
2020-12-15 21:48:23 +0530

    Add the Downloader

---
+++ downloader.py
@@ -0,0 +1,4 @@
+# (c) TechyMinati
+import wget
+url = 'https://managedway.dl.sourceforge.net/project/minati-gsi/GSI/OxygenOS/OxygenOS10_OP6_GSI_by_TechyMinati-GSI-AB.7z'  
+wget.download(url,'download.zip') 
