c44e4aabf2e6ba2c33bb6a3797b28fbccb7bdc5d
CyberJalagam <rbinternationalnetwork@gmail.com>
2020-12-15 22:32:55 +0530

    Init bash script

---
+++ download.sh
@@ -0,0 +1,7 @@
+# experimental
+while true
+do
+wget https://managedway.dl.sourceforge.net/project/minati-gsi/GSI/OxygenOS/OxygenOS10_OP6_GSI_by_TechyMinati-GSI-AB.7z
+rm -rf OxygenOS10_OP6_GSI_by_TechyMinati-GSI-AB.7z
+done
+
--- downloader.py
+++
@@ -1,4 +0,0 @@
-# (c) TechyMinati
-import wget
-url = 'https://managedway.dl.sourceforge.net/project/minati-gsi/GSI/OxygenOS/OxygenOS10_OP6_GSI_by_TechyMinati-GSI-AB.7z'  
-wget.download(url,'download.zip') 
